27c2d9288bf6b5e3ac1aead8fe3784c58f91e073
Bruno Marques <brunomn95@gmail.com>
2020-03-12 19:13:56 -0300

    Add ArticleList, ArticleDetail and ApiRoot views

--- backend/src/articles/views.py
+++ backend/src/articles/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+from articles import serializers
+from articles.models import Article
+
+
+class ArticlesApiRoot(generics.GenericAPIView):
+    name = "articles-api-root"
+    queryset = Article.objects.none()
+
+    def get(self, request, *args, **kwargs):
+        return Response({"articles": reverse(ArticleList.name, request=request),})
+
+
+class ArticleList(generics.ListAPIView):
+    queryset = Article.objects.all().order_by("-publish_date")
+    serializer_class = serializers.ArticleListSerializer
+    PageNumberPagination.page_size = 10
+    name = "article-list"
+
+
+class ArticleDetail(generics.RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = serializers.ArticleDetailSerializer
+    name = "article-detail"
