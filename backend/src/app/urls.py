08ecd63593697f17830bf9bc4f4e673b020e131f
Bruno Marques <brunomn95@gmail.com>
2020-03-13 13:52:24 -0300

    Config main urls.py to use api app with versioning

--- backend/src/app/urls.py
+++ backend/src/app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # Enables the DRF browsable API page
     path("v1/api-auth/", include("rest_framework.urls", namespace="rest_framework_v1")),
-    path("v1/", include(("articles.urls", "articles"), namespace="v1")),
     path("v2/api-auth/", include("rest_framework.urls", namespace="rest_framework_v2")),
-    path("v2/", include(("articles.v2.urls", "articles.v2"), namespace="v2")),
+    path("v1/", include(("api.urls", "api"), namespace="v1")),
+    path("v2/", include(("api.v2.urls", "api.v2"), namespace="v2")),
 ]
