8d16b9a5d3487a271d8e76d42a5a6718e3696d79
Bruno Marques <brunomn95@gmail.com>
2020-03-16 16:12:44 -0300

    Add Title atom

---
+++ frontend/src/components/atoms/title/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './styles.css'
+
+export const TitleSize = {
+  MEDIUM: 'medium',
+  LARGE: 'large',
+}
+
+const Title = props => {
+  const { children, size, className } = props
+  const classProps = `${styles.title} ${styles[size]} ${className}`
+  return <h1 className={classProps}>{children}</h1>
+}
+
+Title.defaultProps = {
+  size: TitleSize.MEDIUM,
+  className: '',
+  children: '',
+}
+
+export default Title
