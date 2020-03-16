61b8939a91e514611a7be0b8de75f3edbe6ccb8e
Bruno Marques <brunomn95@gmail.com>
2020-03-16 17:46:59 -0300

    Add sizing to AuthorImage

--- frontend/src/components/atoms/author-image/index.js
+++ frontend/src/components/atoms/author-image/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import styles from './styles.css'
 
+export const AuthorImageSize = {
+  MEDIUM: 'medium',
+  LARGE: 'large',
+}
+
 const AuthorImage = props => {
-  const { children, image, className } = props
+  const { children, image, size, className } = props
   const classProps = `${styles['author-image']} ${className}`
   return (
     <div style={{ backgroundImage: `url(${image})` }} className={classProps}>
@@ -15,6 +20,7 @@ AuthorImage.defaultProps = {
   className: '',
   children: '',
   image: '',
+  size: AuthorImageSize.MEDIUM,
 }
 
 export default AuthorImage
