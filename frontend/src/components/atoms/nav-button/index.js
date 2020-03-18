2ec67113d7bfa3cf61bec381de5df902d4b2d5fe
Bruno Marques <brunomn95@gmail.com>
2020-03-17 21:45:35 -0300

    Add NavButton, responsible to expand NavBar

---
+++ frontend/src/components/atoms/nav-button/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './styles.css'
+
+import navButton from '_images/menu.png'
+import navButton2x from '_images/menu@2x.png'
+
+const isRetina = (first, second) => {
+  if (window.devicePixelRatio >= 2) {
+    return second
+  }
+  return first
+}
+
+const NavButton = props => {
+  const { className, onClick } = props
+  const classProps = `${styles['nav-button']} ${className}`
+  return (
+    <img
+      className={classProps}
+      src={isRetina(navButton, navButton2x)}
+      alt="Nav Buton"
+      onClick={onClick}></img>
+  )
+}
+
+NavButton.defaultProps = {
+  className: '',
+  onClick: () => {},
+}
+
+export default NavButton
