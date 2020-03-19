34b8ad1aca23173e8a8caddebf6c2726020b9cbb
Bruno Marques <brunomn95@gmail.com>
2020-03-19 00:53:38 -0300

    Remove ArticleApiRoot, change BySubjList response
    The ApiRoot endpoint is now centralized in api app.
    
    Change the return of BySubjectArticleList. Was getting 404 when no
    articles with subject params. Now returns a none object.

--- backend/src/articles/views.py
+++ backend/src/articles/views.py
@@ -10,26 +10,6 @@ from articles.models import Article
 from subjects.models import Subject
 
 
-class ArticleApiRoot(generics.GenericAPIView):
-    name = "article-api-root"
-    queryset = Article.objects.none()
-
-    def get(self, request, *args, **kwargs):
-        return Response(
-            {
-                "articles": reverse(ArticleList.name, request=request),
-                "by-subject-articles": reverse(
-                    BySubjectArticleList.name,
-                    kwargs={"subject": "subject"},
-                    request=request,
-                ),
-                "article-detail": reverse(
-                    ArticleDetail.name, kwargs={"pk": "0"}, request=request
-                ),
-            }
-        )
-
-
 class ArticleList(generics.ListAPIView):
     queryset = Article.objects.all().order_by("-publish_date")
     serializer_class = serializers.ArticleListSerializer
@@ -43,7 +23,9 @@ class BySubjectArticleList(generics.ListAPIView):
     name = "subject-article-list"
 
     def get_queryset(self):
-        self.subject = get_object_or_404(Subject, name=self.kwargs["subject"])
+        if not Subject.objects.filter(name=self.kwargs["subject"]).exists():
+            return Article.objects.none()
+        self.subject = Subject.objects.filter(name=self.kwargs["subject"])
         return Article.objects.filter(subject=self.subject).order_by("-publish_date")
 
 
