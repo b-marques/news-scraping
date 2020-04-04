a0f7e3c14cca49e6d95c84c217624b1a890d7926
Bruno Marques <brunomn95@gmail.com>
2020-04-04 12:07:55 -0300

    refactor(link): remove text decoration

--- frontend/src/components/atoms/link/index.js
+++ frontend/src/components/atoms/link/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link as ReactRouterLink } from 'react-router-dom'
 
+import styles from './styles.css'
+
 const Link = props => {
   const { children, className, href, onClick } = props
+  const classProps = `${styles.link} ${className}`
   return (
-    <ReactRouterLink className={className} to={href} onClick={onClick}>
+    <ReactRouterLink className={classProps} to={href} onClick={onClick}>
       {children}
     </ReactRouterLink>
   )
