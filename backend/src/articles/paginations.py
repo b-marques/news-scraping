ef766c26b7c6eefaa1740fa6f9be3f17cdf8988a
Bruno Marques <brunomn95@gmail.com>
2020-04-06 13:47:05 -0300

    fix(pagination): add page_size to response

--- backend/src/articles/paginations.py
+++ backend/src/articles/paginations.py
@@ -1,5 +1,15 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class ArticlePagination(PageNumberPagination):
     page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'count': self.page.paginator.count,
+            'limit': self.page_size,
+            'results': data
+        })
